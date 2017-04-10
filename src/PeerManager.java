c1ae85201d896360db7e3132e90033d9268222e4
Harika <harikasharma93@gmail.com>
2017-04-09 23:53:46 -0400

    testing printlns7

--- src/PeerManager.java
+++ src/PeerManager.java
@@ -462,9 +462,9 @@ public class PeerManager {
 			byte[] ownerBitField = getOwnerBitField();
 
 			// create the actual message by merging message length, type and payload
-			//byte[] actualMessage = getOriginalMessage(ownerBitField, OriginalMessageTypes.BITFIELD);
+			byte[] actualMessage = getOriginalMessage(ownerBitField, OriginalMessageTypes.BITFIELD);
 
-			byte[] actualMessage = {0,0,0,2,5,-8};
+			//byte[] actualMessage = {0,0,0,2,5,-8};
 			// write the actual message in the peer socket's output stream 
 			output.write(actualMessage);
 			System.out.println("see port for send bit:"+this.socket.getPort());
